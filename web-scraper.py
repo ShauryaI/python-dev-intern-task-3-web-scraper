94027541b10993a7b0927153d1ad0bf1f193344b
Aishwarya Ingole <135330242+ShauryaI@users.noreply.github.com>
2025-06-27 17:57:30 +0530

    Update web-scraper.py
    Undo web-scraper.py

--- web-scraper.py
+++ web-scraper.py
@@ -1,46 +1,33 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-import json
+import requests
+from bs4 import BeautifulSoup
 from datetime import datetime
 
 current_datetime = datetime.now()
 formatted_datetime = current_datetime.strftime("%Y-%m-%d %H-%M-%S")
-TEXT_FILE_NAME = "REUTERS_" + formatted_datetime + ".txt"
+TEXT_FILE_NAME = "TOI_" + formatted_datetime + ".txt"
 
 def web_scraper():
+    url = "https://timesofindia.indiatimes.com/home/headlines"
     try:
-        # Initialize the WebDriver
-        driver = webdriver.Chrome(service=Service())
+        response = requests.get(url)
+        response.raise_for_status() #If an error occurs, this method returns an HTTP Error object
+        print("Request successful!")
 
-        # URL of the Reuters article
-        url = "https://www.reuters.com/world/europe/excitement-relief-paris-notre-dame-cathedral-prepares-reopen-2024-11-29/"
+        soup = BeautifulSoup(response.content, "html.parser")
 
-        # Open the URL in the browser
-        driver.get(url)
+        headline_elements = soup.find_all("span", class_="w_tle")
 
-        # Extract the title from the <h1> tag
-        title_element = driver.find_element(By.CSS_SELECTOR, "h1")
-        title = title_element.text
+        with open(TEXT_FILE_NAME, "w", encoding="utf-8") as text_file:
+            for index, headline in enumerate(headline_elements, start=1):
+                text_file.write(f"{index}. {headline.text}\n")
+            else:
+                print(f"For Top News Headlines, please check file: {TEXT_FILE_NAME}")
 
-        # Select all text elements
-        paragraph_elements = driver.find_elements(By.CSS_SELECTOR, "[data-testid^='paragraph-']")
-
-        # Aggregate their text
-        content = " ".join(p.text for p in paragraph_elements)
-
-        # Prepare the data to be exported as JSON
-        article = {
-            "title": title,
-            "content": content
-        }
-
-        # Export data to a JSON file
-        with open("article.json", "w", encoding="utf-8") as json_file:
-            json.dump(article, json_file, ensure_ascii=False, indent=4)
-
-    except Exception as e:
+    except (requests.exceptions.ConnectionError, requests.exceptions.Timeout) as e:
+        print("A connection error or timeout occurred:", e)
+    except requests.exceptions.HTTPError as e:
+        print("HTTP Error:", e)
+    except requests.exceptions.RequestException as e:
         print("An error occurred:", e)
 
 def main():
